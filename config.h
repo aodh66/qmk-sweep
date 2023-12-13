ea272094f9c2d39c393837ae2e1b82adff8ad4b2
Aidan Meehan <aodhan66@gmail.com>
2023-12-13 15:46:32 +0000

    initialised git

---
+++ config.h
@@ -0,0 +1,9 @@
+#pragma once
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 180
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
+
+//Combo stuff
+#define COMBO_COUNT 17
+
---
+++ rules.mk
@@ -0,0 +1,7 @@
+# This file intentionally left blank
+CAPS_WORD_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+UNICODE_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+SRC += features/achordion.c
