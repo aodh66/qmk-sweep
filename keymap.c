d391b34874d6746b7a94bcfb53e5f75e1527e4d6
Aidan Meehan <aodhan66@gmail.com>
2023-12-15 16:27:01 +0000

    put semicolon back on querty alpha layer so it can be picked up correctly and converted when practicing recurva

--- keymap.c
+++ keymap.c
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[2] = LAYOUT_split_3x5_2(
 		SS_QU		, KC_W		  , KC_E		, KC_R		  , KC_T	  	 , 			KC_Y  		 , KC_U		   , KC_I   	 , KC_O  	   , KC_P		 , 
 		LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G	  	 , 			KC_H 		 , RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), 
-		KC_Z		, KC_X		  , KC_C		, KC_V		  , KC_B	  	 , 			KC_N   		 , KC_M		   , KC_COMM	 , KC_DOT	   , KC_SLSH	 , 
+		KC_Z		, KC_X		  , KC_C		, KC_V		  , KC_B	  	 , 			KC_N   		 , KC_M		   , KC_COMM	 , KC_DOT	   , KC_SCLN	 , 
 			   								   	  MO(3)		  , LT(5, KC_SPC), 			LCTL(KC_BSPC), MO(4))	   ,
 	
 	//Nav/Ext
