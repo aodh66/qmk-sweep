3a9d0280e94322f6ab36500bc7f3dcfaba211644
Aidan Meehan <aodhan66@gmail.com>
2024-06-22 21:39:55 +0100

    switched home row mods to bottom row

--- keymap.c
+++ keymap.c
@@ -12,11 +12,21 @@
 #define HOME_T LSFT_T(KC_T)
 #define HOME_C LCTL_T(KC_C)
 
+#define HOME_Z LGUI_T(KC_Z)
+#define HOME_X LALT_T(KC_X)
+#define HOME_K LSFT_T(KC_K)
+#define HOME_G LCTL_T(KC_G)
+
 // Right-hand
 #define HOME_H RCTL_T(KC_H)
 #define HOME_E RSFT_T(KC_E)
 #define HOME_A LALT_T(KC_A)
 #define HOME_I RGUI_T(KC_I)
+
+#define HOME_L RCTL_T(KC_L)
+#define HOME_SLSH RSFT_T(KC_SLSH)
+#define HOME_QUOT LALT_T(KC_QUOT)
+#define HOME_COMM RGUI_T(KC_COMM)
 
 // * ------------------
 // * -- Tapping Term --
@@ -114,8 +124,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                 unregister_mods(MOD_MASK_CSAG);
                 if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
                     SEND_STRING("{}");
-                } else if ((mods | oneshot_mods) & MOD_MASK_CTRL) {
+                } else if ((mods | oneshot_mods) & MOD_MASK_ALT) {
                     SEND_STRING("<>");
+                } else if ((mods | oneshot_mods) & MOD_MASK_CTRL) {
+                    SEND_STRING("[]");
                 } else {
                     SEND_STRING("()");
                 }
@@ -160,8 +172,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base (Recurva Colstag)
     [0] = LAYOUT_split_3x5_2(
         KC_F, KC_R, KC_D, KC_P, KC_V,                                       SS_QU, KC_M, KC_U, KC_O, KC_Y, 
-        HOME_S, HOME_N, HOME_T, HOME_C, KC_B,                               KC_DOT, HOME_H, HOME_E, HOME_A, HOME_I, 
-        KC_Z, KC_X, KC_K, KC_G, KC_W,                                       KC_J, KC_L, KC_SLSH, KC_QUOT, KC_COMM, 
+        KC_S, KC_N, KC_T, KC_C, KC_B,                               KC_DOT, KC_H,  KC_E,  KC_A,  KC_I,
+        HOME_Z, HOME_X, HOME_K, HOME_G, KC_W,                                       KC_J,   HOME_L,  HOME_SLSH, HOME_QUOT, HOME_COMM,
         MO(1), LT(3, KC_SPC),                                               LCTL(KC_BSPC), MO(2)),
 
     // Nav/Ext
@@ -174,13 +186,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Symbol
     [2] = LAYOUT_split_3x5_2(
         KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                           KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_PIPE, 
-        KC_GRV, KC_LT, KC_LCBR, KC_LPRN, KC_LBRC,                           KC_COLN, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, 
-        KC_TILD, KC_GT, KC_RCBR, KC_RPRN, KC_RBRC,                          KC_SCLN, KC_MINS, KC_EQL, KC_PLUS, KC_BSLS, 
+        KC_GRV, KC_LT, KC_LCBR, KC_LPRN, KC_LBRC,                           KC_EQL, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, 
+        KC_TILD, KC_GT, KC_RCBR, KC_RPRN, KC_RBRC,                          KC_PLUS, KC_MINS, KC_SCLN, KC_COLN, KC_BSLS,
         MO(4), KC_ENT,                                                      KC_NO, KC_TRNS),
 
     // Numpad
     [3] = LAYOUT_split_3x5_2(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_MINS, KC_7, KC_8, KC_9, KC_NO, 
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_MINS, KC_7, KC_8, KC_9, KC_ASTR, 
         KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO,                          KC_DOT, KC_4, KC_5, KC_6, KC_0, 
         KC_NO, KC_NO, KC_BSPC, KC_DEL, KC_NO,                               KC_PLUS, KC_1, KC_2, KC_3, KC_SLSH, 
         KC_NO, KC_TRNS,                                                     KC_BSPC, KC_DEL),
@@ -201,25 +213,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM combo1[] = {SS_QU, KC_U, COMBO_END}; // Q+U -> Q
 const uint16_t PROGMEM combo17[] = {SS_QU, KC_M, COMBO_END}; // Q+M -> Q
 
-const uint16_t PROGMEM combo2[] = {KC_K, KC_G, COMBO_END}; // K+G -> ESC
-const uint16_t PROGMEM combo3[] = {KC_X, KC_K, COMBO_END}; // X+K -> Tab
+const uint16_t PROGMEM combo2[] = {HOME_K, HOME_G, COMBO_END}; // K+G -> ESC
+const uint16_t PROGMEM combo3[] = {HOME_X, HOME_K, COMBO_END}; // X+K -> Tab
 
-const uint16_t PROGMEM combo4[] = {KC_F, HOME_S, COMBO_END}; // F+S -> Grave `
-const uint16_t PROGMEM combo5[] = {KC_R, HOME_N, COMBO_END}; // R+N -> Less than <
-const uint16_t PROGMEM combo6[] = {KC_D, HOME_T, COMBO_END}; // D+T -> Left Curly Brace {
-const uint16_t PROGMEM combo7[] = {KC_P, HOME_C, COMBO_END}; // P+C -> Left Parenthesis (
-const uint16_t PROGMEM combo8[] = {KC_V, KC_B, COMBO_END};   // V+B -> Left Brace [
+// const uint16_t PROGMEM combo4[] = {KC_F, HOME_S, COMBO_END}; // F+S -> Grave `
+// const uint16_t PROGMEM combo5[] = {KC_R, HOME_N, COMBO_END}; // R+N -> Less than <
+// const uint16_t PROGMEM combo6[] = {KC_D, HOME_T, COMBO_END}; // D+T -> Left Curly Brace {
+// const uint16_t PROGMEM combo7[] = {KC_P, HOME_C, COMBO_END}; // P+C -> Left Parenthesis (
+// const uint16_t PROGMEM combo8[] = {KC_V, KC_B, COMBO_END};   // V+B -> Left Brace [
 
 // Right Hand
 const uint16_t PROGMEM combo9[] = {KC_SLSH, KC_QUOT, COMBO_END}; // /+' -> Enter
-const uint16_t PROGMEM combo10[] = {KC_B, KC_DOT, COMBO_END};     // B+. -> Caps Word
-const uint16_t PROGMEM combo11[] = {KC_L, KC_SLSH, COMBO_END};    // L+/ -> Delete Bot
+const uint16_t PROGMEM combo10[] = {HOME_G, HOME_L, COMBO_END};     // G+L -> Caps Word
+const uint16_t PROGMEM combo11[] = {HOME_L, HOME_SLSH, COMBO_END};    // L+/ -> Delete Bot
 
-const uint16_t PROGMEM combo12[] = {SS_QU, KC_DOT, COMBO_END}; // Q+. -> Right Brace ]
-const uint16_t PROGMEM combo13[] = {KC_M, HOME_H, COMBO_END};  // M+H -> Right Parenthesis )
-const uint16_t PROGMEM combo14[] = {KC_U, HOME_E, COMBO_END};  // U+E -> Right Curly Brace }
-const uint16_t PROGMEM combo15[] = {KC_O, HOME_A, COMBO_END};  // O+A -> Greater than >
-const uint16_t PROGMEM combo16[] = {KC_Y, HOME_I, COMBO_END};  // Y+I -> Semicolon ;
+// const uint16_t PROGMEM combo12[] = {SS_QU, KC_DOT, COMBO_END}; // Q+. -> Right Brace ]
+// const uint16_t PROGMEM combo13[] = {KC_M, HOME_H, COMBO_END};  // M+H -> Right Parenthesis )
+// const uint16_t PROGMEM combo14[] = {KC_U, HOME_E, COMBO_END};  // U+E -> Right Curly Brace }
+// const uint16_t PROGMEM combo15[] = {KC_O, HOME_A, COMBO_END};  // O+A -> Greater than >
+// const uint16_t PROGMEM combo16[] = {KC_Y, HOME_I, COMBO_END};  // Y+I -> Semicolon ;
 
 // Outcome Definitions
 combo_t key_combos[COMBO_COUNT] = {
@@ -230,20 +242,20 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo2, KC_ESC), // K+G -> ESC
     COMBO(combo3, KC_TAB), // X+K -> Tab
 
-    COMBO(combo4, KC_GRV),  // F+S -> Grave `
-    COMBO(combo5, KC_LT),   // R+N -> Less than <
-    COMBO(combo6, KC_LCBR), // D+T -> Left Curly Brace {
-    COMBO(combo7, KC_LPRN), // P+C -> Left Parenthesis (
-    COMBO(combo8, KC_LBRC), // V+B -> Left Brace [
+    // COMBO(combo4, KC_GRV),  // F+S -> Grave `
+    // COMBO(combo5, KC_LT),   // R+N -> Less than <
+    // COMBO(combo6, KC_LCBR), // D+T -> Left Curly Brace {
+    // COMBO(combo7, KC_LPRN), // P+C -> Left Parenthesis (
+    // COMBO(combo8, KC_LBRC), // V+B -> Left Brace [
 
     // Right Hand
     COMBO(combo9, KC_ENT),  // /+' -> Enter
-    COMBO(combo10, CW_TOGG), // B+. -> Caps Word
+    COMBO(combo10, CW_TOGG), // G+L -> Caps Word
     COMBO(combo11, KC_DEL),  // L+/ -> Delete Bot
 
-    COMBO(combo12, KC_RBRC), // Q+. -> Right Brace ]
-    COMBO(combo13, KC_RPRN), // M+H -> Right Parenthesis )
-    COMBO(combo14, KC_RCBR), // U+E -> Right Curly Brace }
-    COMBO(combo15, KC_GT),   // O+A -> Greater than >
-    COMBO(combo16, KC_SCLN), // Y+I -> Semicolon ;
+    // COMBO(combo12, KC_RBRC), // Q+. -> Right Brace ]
+    // COMBO(combo13, KC_RPRN), // M+H -> Right Parenthesis )
+    // COMBO(combo14, KC_RCBR), // U+E -> Right Curly Brace }
+    // COMBO(combo15, KC_GT),   // O+A -> Greater than >
+    // COMBO(combo16, KC_SCLN), // Y+I -> Semicolon ;
 };
